601d58d2da11f07aef74f9906876e0417425b85a
Vital <vitaly.kucheruk@goritchy.com>
2017-12-01 11:15:22 +0200

    ref

--- TestVSSolution/Program.cs
+++ TestVSSolution/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +13,9 @@ namespace TestVSSolution
     {
         static void Main(string[] args)
         {
-            Console.Write("Hi! Huy sdfsdfsdfsdfdsfdfdsf rrrrr");
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+
+            Console.Write($"Hi! Huy {version.Build} {version.Major} {version.Minor} {version.Revision}");
             Console.ReadLine();
             Console.ReadLine();
         }
