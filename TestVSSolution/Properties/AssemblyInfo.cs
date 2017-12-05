da05376ec572ebc3068ba2ce864bd733cd41b85e
Vital <vitaly.kucheruk@goritchy.com>
2017-12-05 15:58:51 +0200

    reffgh1111

--- TestVSSolution/Properties/AssemblyInfo.cs
+++ TestVSSolution/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.5.11")]
-[assembly: AssemblyFileVersion("2.3.4.6")]
+[assembly: AssemblyFileVersion("2.3.4.8")]
